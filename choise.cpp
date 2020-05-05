86a76e37bc0fb6177d82c26771335f2ad7e773f6
ololo534 <vanekkravtsov@mail.ru>
2020-05-05 23:05:26 +1000

    logic of game

--- choise.cpp
+++ choise.cpp
@@ -1,6 +1,7 @@
 #include "choise.h"
 #include "ui_choise.h"
 #include "QMessageBox"
+#include <QDebug>
 
 Choise::Choise(QWidget *parent) :
     QDialog(parent),
@@ -15,22 +16,58 @@ Choise::~Choise()
 }
 
 void Choise::on_GoButton_clicked() {
+    //Выбор элемета компьютера
+    int i = qrand() % ((3 + 0) - 0) + 0;
+
+    //Выбор человеком
     if ( ui->RockButton->isChecked() ) {
-        QMessageBox msg(QMessageBox::Information,"Info","Your choice: Rock", QMessageBox::Ok, nullptr);
-        msg.exec();
-    }
-    if ( ui->PaperButton->isChecked() ) {
-        QMessageBox msg(QMessageBox::Information,"Info","Your choice: Paper", QMessageBox::Ok, nullptr);
-        msg.exec();
+        personMove = 1;
     }
     if ( ui->ScissorsButton->isChecked() ) {
-        QMessageBox msg(QMessageBox::Information,"Info","Your choice: Scissors", QMessageBox::Ok, nullptr);
+        personMove = 2;
+    }
+    if ( ui->PaperButton->isChecked() ) {
+        personMove = 3;
+    }
+    computerMove = computerMoves.at(i);
+    --personMove;
+
+    //определение результата
+    short result = (computerMove - personMove) % 3;
+
+    //Вывод результата
+    switch (result < 0 ? result + 3 : result) {
+    case 0:{
+        QMessageBox msg(QMessageBox::Information,"Info","Draw\nYour choise: " + nameMoves.at(personMove)
+                        + "\nComputer choise: "+ nameMoves.at(computerMove), QMessageBox::Ok, nullptr);
         msg.exec();
+        ++countDraw;
+        break;
+    }
+    case 1:{
+        QMessageBox msg(QMessageBox::Information,"Info","Win\nYour choise: " + nameMoves.at(personMove)
+                        + "\nComputer choise: "+ nameMoves.at(computerMove), QMessageBox::Ok, nullptr);
+        msg.exec();
+        ++countPlayerWins;
+        break;
+    }
+    case 2:{
+        QMessageBox msg(QMessageBox::Information,"Info","Lose\nYour choise: " + nameMoves.at(personMove)
+                        + "\nComputer choise: "+ nameMoves.at(computerMove), QMessageBox::Ok, nullptr);
+        msg.exec();
+        ++countComputerWins;
+        break;
+    }
+
     }
 }
 
 void Choise::on_ExitButton_clicked() {
-    QMessageBox msg(QMessageBox::Information,"Statistics","Ваша статистика: ", QMessageBox::Ok, nullptr);
+    //Вывод статистики
+    QMessageBox msg(QMessageBox::Information,"Statistics",
+                    "Your statistics: \nComputer win: " + QString::number(countComputerWins) +
+                    " times\nYou win: " + QString::number(countPlayerWins) +
+                    " times\nDraw: " + QString::number(countDraw) + " times",
+                    QMessageBox::Ok, nullptr);
     msg.exec();
-    Choise::hide();
 }
--- choise.h
+++ choise.h
@@ -2,6 +2,8 @@
 #define CHOISE_H
 
 #include <QDialog>
+#include <QVector>
+#include <QString>
 
 namespace Ui {
 class Choise;
@@ -14,6 +16,18 @@ class Choise : public QDialog
 public:
     explicit Choise(QWidget *parent = nullptr);
     ~Choise();
+    size_t countDraw{ 0 };
+    size_t countComputerWins{ 0 };
+    size_t countPlayerWins{ 0 };
+
+    enum Moves { ROCK, SCISSORS, PAPER };
+
+    QVector<Moves> computerMoves {ROCK, PAPER, SCISSORS};
+
+    QVector<QString> nameMoves{ "Rock", "Scissors", "Paper" };
+
+    short personMove{ 0 };
+    short computerMove{ 0 };
 
 private slots:
     void on_ExitButton_clicked();
